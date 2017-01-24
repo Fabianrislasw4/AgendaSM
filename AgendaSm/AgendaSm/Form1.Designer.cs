7a6740f0a8900a25e22130f470f896aaece58018
Fabianrislasw4 <fabianislasromo@gmail.com>
2017-01-24 16:04:38 -0700

    Agregue lo que revisamos.

--- AgendaSm/AgendaSm/Form1.Designer.cs
+++ AgendaSm/AgendaSm/Form1.Designer.cs
@@ -28,58 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "1v1 en Lol o que ? ";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 63);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 69);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Puro Maaincra alavrg";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(15, 138);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(166, 69);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Puro Maaincra alavrg";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
 
     }
 }
