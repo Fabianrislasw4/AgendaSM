6fe369b024c4a55220ba332d7e131127bb908400
AaronGraciaxD <maincra828@gmail.com>
2017-01-23 21:35:54 -0700

    FabManco2

--- AgendaSm/AgendaSm/Form1.Designer.cs
+++ AgendaSm/AgendaSm/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -47,14 +48,24 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(166, 69);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Puro MAaincra alavrg";
+            this.button1.Text = "Puro Maaincra alavrg";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 69);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Puro Maaincra alavrg";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
 
     }
 }
